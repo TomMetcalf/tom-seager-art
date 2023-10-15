dc58e1454ae69e2a355f50fe06be48529e9a9dfa
Tom Metcalf <intermet@gmail.com>
2023-10-15 11:43:09 +0100

    add initial site setup files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>art.tom-seager.com</h1>
+      <h2>The Home of Tom Seager Art</h2>
+      <p>Site currently under construction.</p>
+      <p>Please check back for future updates.</p>
+      <section className="siteLinks">
+        <a
+          href="https://www.tom-seager.com"
+          className="siteLinkStyle"
+          target="_blank"
+        >
+          Return to Tom Seager Main Site
+        </a>
+      </section>
+    </>
+  );
+}
+
+export default App;
