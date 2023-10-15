9da68188c7940818cafd10c1acd037d60a06aac2
Tom Metcalf <intermet@gmail.com>
2023-10-15 12:32:10 +0100

    make text reposive for smaller devices

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>art.tom-seager.com</h1>
+      <h1 className='web-address'>art.tom-seager.com</h1>
       <h2>
         <span className="first-line">The Home of </span>
         <span className="second-line">Tom Seager Art</span>
