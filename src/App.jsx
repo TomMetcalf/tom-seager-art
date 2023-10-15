a84136caa167887d21351dd7a79131542fab6b38
Tom Metcalf <intermet@gmail.com>
2023-10-15 14:59:24 +0100

    add social icon links

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,20 @@ function App() {
           Return to the Tom Seager Main Site
         </a>
       </section>
+      <section className="social-links">
+        <div className="social-items">
+          <a href="https://www.instagram.com/tomseagerart/" target="_blank">
+            <i className="fa-brands fa-instagram instagram-icon"></i>
+          <p>Tom Seager Art</p>
+          </a>
+        </div>
+        <div className="social-items">
+          <a href="https://www.instagram.com/carvefinart/" target="_blank">
+            <i className="fa-brands fa-instagram instagram-icon"></i>
+          <p>Carve Fin Art</p>
+          </a>
+        </div>
+      </section>
     </>
   );
 }
