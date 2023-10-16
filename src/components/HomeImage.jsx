fd89a5e8cc21979e2e07258c11ab8a2f7abc872c
Tom Metcalf <intermet@gmail.com>
2023-10-16 19:40:10 +0100

    test ar on image

--- src/components/HomeImage.jsx
+++ src/components/HomeImage.jsx
@@ -2,6 +2,16 @@ import stillImage from '../assets/images/TomSeager_StillImage_OscarLangburne.jpg
 
 export default function HomeImage() {
   return (
-    <img className='home-image' src={stillImage} alt="Black outline of surfer on a white background" />
+    <section>
+      <img
+        className="home-image"
+        src={stillImage}
+        alt="Black outline of surfer on a white background"
+      />
+      <br />
+      <a href="https://www.tom-seager.com/surfer-ar.html" target="_blank">
+        Launch Camera
+      </a>
+    </section>
   );
 }
