b9839f98bcb2172500fc9a865ee7c752ee74872a
Tom Metcalf <intermet@gmail.com>
2023-10-17 19:43:07 +0100

    add second ar trigger image

--- src/components/HomeImage.jsx
+++ src/components/HomeImage.jsx
@@ -1,14 +1,29 @@
-import stillImage from '../assets/images/TomSeager_StillImage_JordySmith.jpg';
+import jordyImage from '../assets/images/TomSeager_StillImage_JordySmith.jpg';
+import tiaImage from '../assets/images/TomSeager_StillImage_TiaBlanco.jpg';
 
 export default function HomeImage() {
   return (
-    <section className='home-image-section'>
-      <img
-        className="home-image"
-        src={stillImage}
-        alt="Black outline of surfer on a white background"
-      />
-       <a href="https://ar.tom-seager.com" target="_blank">Launch Camera</a>
-    </section>
+    <>
+      <section className="home-image-section">
+        <img
+          className="home-image"
+          src={jordyImage}
+          alt="digital drawings of a male surfer in a black wetsuit in various positions of a turn moving from right to left across the image"
+        />
+        <a href="https://ar.tom-seager.com" target="_blank">
+          Launch Camera
+        </a>
+      </section>
+      <section className="home-image-section">
+        <img
+          className="home-image"
+          src={tiaImage}
+          alt="digital drawings of a female surfer in  pink wetsuit in various positions of a turn moving from left to right across the image"
+        />
+        <a href="https://ar.tom-seager.com" target="_blank">
+          Launch Camera
+        </a>
+      </section>
+    </>
   );
 }
