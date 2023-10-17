3d9d9bef308bb21035a2b5b4131c23f6cf81baf3
Tom Metcalf <intermet@gmail.com>
2023-10-17 18:04:23 +0100

    setup image and launch camera link

--- src/components/HomeImage.jsx
+++ src/components/HomeImage.jsx
@@ -1,13 +1,14 @@
-import stillImage from '../assets/images/TomSeager_StillImage_OscarLangburne.jpg';
+import stillImage from '../assets/images/TomSeager_StillImage_JordySmith.jpg';
 
 export default function HomeImage() {
   return (
-    <section>
+    <section className='home-image-section'>
       <img
         className="home-image"
         src={stillImage}
         alt="Black outline of surfer on a white background"
       />
+       <a href="https://ar.tom-seager.com" target="_blank">Launch Camera</a>
     </section>
   );
 }
